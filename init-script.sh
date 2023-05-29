93cf624e653f9cebb7ede2033c55ce883dc6a5a9
AxelRonny <127845742+AxelRonny@users.noreply.github.com>
2023-05-29 22:30:12 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 #Installing Docker
-...
-...
+
+sudo apt-get update
+sudo apt-get install -y
+curl -fsSL https://get.docker.com/ -o get-docker.sh
+sudo sh get-docker.sh
 
 #Creating container
-...
-...
+
+sudo wget https://hub.docker.com/layers/axelronny/nginx1/latest/images
+sudo docker build  -t nginx1 .
+docker run --name axelconteneurnginx -d -p 80:80 nginx1
